e5bad1fa092871b0e3058e97245027af311ecd1b
loretta-dhahabu <loreydjefwa@gmail.com>
2022-06-15 22:53:27 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Add your code here
+
+// const submitData = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//     },
+  
+//   body: JSON.Stringify({            //convert objects to strings
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   }),
+
+// };
+// fetch("http://localhost:3000/dogs", configurationObject);
+
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            Accept : "application/json"
+        },
+        body : JSON.stringify( {
+            "name" : userName,
+            "email" : userEmail
+        })
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        const newId = document.createElement("p");
+        newId.textContent = data.id;
+        const bodyText = document.querySelector("body");
+        bodyText.appendChild(newId);
+    })
+    .catch(error => {
+        const errorMsg = document.createElement("p");
+        errorMsg.textContent = error.message;
+        const bodyText = document.querySelector("body");
+        bodyText.appendChild(errorMsg);
+    })
+}
